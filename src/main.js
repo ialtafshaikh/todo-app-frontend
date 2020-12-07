2865247dac0bc1b16442d28143929dcc06376950
ialtafshaikh <altaf.shaikh@contentstack.com>
2020-12-07 10:35:17 +0530

    todo filter helper func added

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import { formSubmission } from "./helper/formSubmission.js";
 import { deleteCheck } from "./helper/deleteCheck.js";
 import { renderTodo } from "./helper/renderTodo.js";
+import { filterTodo } from "./helper/filterTodo.js";
 import { endpoint } from "./endpoints.js";
 
 window.onload = () => {
   document.forms.taskList.addEventListener("submit", formSubmission);
   document.getElementById("todo-list").addEventListener("click", deleteCheck);
-  //   document.getElementById("todo-filter").addEventListener("change", filterTodo);
+  document.getElementById("todo-filter").addEventListener("change", filterTodo);
 };
 
 //onRefresh => check if todos are present or not
