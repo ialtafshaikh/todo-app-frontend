1da6808ea60ffe0c0eec8ac40dc964a2606d1acb
ialtafshaikh <altaf.shaikh@contentstack.com>
2020-12-07 08:38:41 +0530

    renderTodo helper fun added

--- src/helper/createTodo.js
+++ src/helper/createTodo.js
@@ -4,17 +4,7 @@ import { renderTodo } from "./renderTodo.js";
 export const createTodo = (text) => {
   //validation
   if (inputValidation(text)) {
-    //create task
-    const todoObj = new taskConstructor(text);
     renderTodo(todoObj);
     //store the task
-    try {
-      const StoredTodos = getStoredTodo();
-      StoredTodos.push(todoObj);
-      storeTodo(StoredTodos);
-    } catch (error) {
-      console.log("Check Your Local Storage");
-      throw new Error("Issue with Browser Local storage");
-    }
   }
 };
