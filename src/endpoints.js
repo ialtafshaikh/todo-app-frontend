0c6ac2976d696f3179417e595061d1d2e2e17ce3
ialtafshaikh <altaf.shaikh@contentstack.com>
2020-12-14 20:16:23 +0530

    set baseurl to hosted todobacked

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,5 +1,5 @@
-// const BASEURL = "https://todo-rest-api-backend.herokuapp.com/";
-const BASEURL = "http://localhost:3000/";
+const BASEURL = "https://todo-rest-api-backend.herokuapp.com/";
+// const BASEURL = "http://localhost:3000/";
 export const endpoint = BASEURL + "todos/";
 export const signup = BASEURL + "signup";
 export const login = BASEURL + "login";
